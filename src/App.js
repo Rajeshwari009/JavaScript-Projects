ad210f5a32bd3da72dade8e7062b23ed48f48118
Rajeshwari009 <155558086+Rajeshwari009@users.noreply.github.com>
2024-03-13 22:41:29 +0530

    Starting React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import './App.css';
+import ExpenseItems from './components/ExpenseItem'; 
+function App() {
+  return (
+    <div>
+      <ExpenseItems> </ExpenseItems> 
+    </div>
+  );
+}
+
+export default App;
