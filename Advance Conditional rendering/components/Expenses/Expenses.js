b9554ec69a7955e0cf625022c2bc22ef49bd2e53
Rajeshwari009 <155558086+Rajeshwari009@users.noreply.github.com>
2024-03-17 14:02:02 +0530

    Advance Conditional rendering

---
+++ Advance Conditional rendering/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      ></ExpensesFilter>
+      <ExpensesList items= {filteredExpenses}/>
+    </Card>
+  );
+}
+
+export default Expenses;
