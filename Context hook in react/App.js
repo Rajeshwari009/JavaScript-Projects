f697c16183fe3ba4a27859433f9a89cc6599bcf8
Rajeshwari009 <155558086+Rajeshwari009@users.noreply.github.com>
2024-03-20 08:38:38 +0530

    Context hook in react

---
+++ Context hook in react/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import Authcontext from "./store/auth-context";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedin");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <Authcontext.Provider value={{
+      isLoggedIn: isLoggedIn,
+    }}>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </Authcontext.Provider>
+  );
+}
+
+export default App;
