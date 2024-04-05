abb09111217091dcc1796e1f08f8e3f37854dad6
Rajeshwari009 <155558086+Rajeshwari009@users.noreply.github.com>
2024-04-06 02:37:10 +0530

    Setting new password via api

---
+++ Setting new password via api/components/Profile/ProfileForm.js
@@ -0,0 +1,45 @@
+import classes from './ProfileForm.module.css';
+import { useRef, useContext} from 'react';
+import AuthContext from '../../Store/auth-context';
+
+const ProfileForm = () => {
+  const authCtx= useContext(AuthContext)
+
+
+const newPasswordInputref= useRef()
+
+
+const submitHandler= (event) => {
+  event.preventDefault();
+  const enteredNewPassword= newPasswordInputref.current.value
+  fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDYEGo9KIc1eCdsIstDVUYm1Ql2cFnsQA8', {
+    method:'POST',
+    body:JSON.stringify({
+      idToken:authCtx.token,
+      password:enteredNewPassword,
+      returnSecureToken: false,
+
+    }),
+    headers:{
+      'Content-Type': 'application/json'
+    }
+
+  })
+
+}
+
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor='new-password'>New Password</label>
+        <input type='password' id='new-password' minLength="7" ref={ newPasswordInputref} />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+}
+
+export default ProfileForm;
